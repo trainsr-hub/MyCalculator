e89f95ef17138ac0c492329a082cb5bb2606a26b
trainsr-hub <duoclust@gmail.com>
2026-02-09 16:02:45 +0700

    Update JurassicWorld.py

--- JurassicWorld.py
+++ JurassicWorld.py
@@ -324,8 +324,8 @@ def tab3():
 
     st.markdown("---")
 
-    Health3 = st.number_input("3rd Health", min_value=0, value=max(BasePower[selected_rank][0] - Main_Health,0), step=50)
-    Attack3 = st.number_input("3rd Attack", min_value=0, value=max(BasePower[selected_rank][1] - Main_Attack, 0), step=25)
+    Health3 = st.number_input("3rd Health", min_value=0, 500, step=50)
+    Attack3 = st.number_input("3rd Attack", min_value=0, value=300, step=25)
 
     show_boxed_text(
         "Remaining",
