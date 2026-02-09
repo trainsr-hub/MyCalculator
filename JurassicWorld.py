5ccbff9cd1ef680a6b6183c8c64a80d9864f3f72
trainsr-hub <duoclust@gmail.com>
2026-02-09 20:56:04 +0700

    Update JurassicWorld.py

--- JurassicWorld.py
+++ JurassicWorld.py
@@ -133,7 +133,7 @@ def show_graph(C, x_point=None, y_point=None, Optimal_x=None):
     # =========================
     # VÙNG RED (> C)
     # =========================
-    ax.fill_between(x, y, y_max, color="purple", alpha=0.7)
+    ax.fill_between(x, y, y_max, color="purple", alpha=0.4)
 
     ax.plot(x, y)
 
