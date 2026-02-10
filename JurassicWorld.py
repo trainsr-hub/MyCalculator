aac0292cb47a6d8f79920d5d1686d12bada04325
trainsr-hub <duoclust@gmail.com>
2026-02-10 17:34:39 +0700

    Update JurassicWorld.py

--- JurassicWorld.py
+++ JurassicWorld.py
@@ -12,25 +12,25 @@ padding:15px;
 border-radius:10px;  
 background-color:#111;  
 margin-top:15px;">  
-  
+
+<div>  
+<span style="display:inline-block;width:15px;height:15px;background:#00ffff;opacity:1;border-radius:3px;"></span>  
+Linh xe ôm  
+</div>  
+
 <div style="margin-bottom:8px;">  
 <span style="display:inline-block;width:15px;height:15px;background:blue;opacity:1;border-radius:3px;"></span>  
+<b> 1 Carry & 1 Support</b> — Cặp đôi hoàn hảo
+</div>  
+  
+<div style="margin-bottom:8px;">  
+<span style="display:inline-block;width:15px;height:15px;background:green;opacity:1;border-radius:3px;"></span>  
 <b> Team 2</b> — Cân bằng nhưng không cân mọi kèo  
 </div>  
   
 <div style="margin-bottom:8px;">  
-<span style="display:inline-block;width:15px;height:15px;background:lime;opacity:1;border-radius:3px;"></span>  
-<b> 1 Carry & 1 Support</b> — Cặp đôi hoàn hảo  
-</div>  
-  
-<div style="margin-bottom:8px;">  
-<span style="display:inline-block;width:15px;height:15px;background:orange;opacity:1;border-radius:3px;"></span>  
+<span style="display:inline-block;width:15px;height:15px;background:yellow;opacity:1;border-radius:3px;"></span>  
 Damage thấp chỉ có tác dụng cảm tử với khắc hệ của Carry  
-</div>  
-  
-<div>  
-<span style="display:inline-block;width:15px;height:15px;background:white;opacity:0.2;border-radius:3px;"></span>  
-Linh xe ôm  
 </div>  
   
 </div>"""  
