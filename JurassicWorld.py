aaeeadb378748e0588a070978adfe55bef6d0197
trainsr-hub <duoclust@gmail.com>
2026-02-11 19:08:15 +0700

    Update JurassicWorld.py

--- JurassicWorld.py
+++ JurassicWorld.py
@@ -116,7 +116,7 @@ def plot_decay_timedelta(Timedeltax, max_x=7, n_times=None):
             ),
             linestyles="solid",
             color="black",
-            alpha=0.3
+            alpha=0
         )
     ax.set_xlim(-0.5, max_x + 0.5)
 
