671aea7eef546871848b931be34e21df035c7d59
trainsr-hub <duoclust@gmail.com>
2026-02-13 10:05:59 +0700

    Update time_utils.py

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,1 +1,11 @@
-h
+from datetime import timedelta
+
+def format_duration(td):
+    total_minutes = int(td.total_seconds() // 60)
+
+    d, rem_min = divmod(total_minutes, 1440)
+    h, m = divmod(rem_min, 60)
+
+    return " ".join(
+        f"{v}{k}" for v, k in [(d, "d"), (h, "h"), (m, "m")] if v
+    ) or "0m"
