65dcab161cfd2a4cdcea9fe5dfc82cf57046c63d
trainsr-hub <duoclust@gmail.com>
2026-02-14 22:14:00 +0700

    Update JurassicWorld.py

--- JurassicWorld.py
+++ JurassicWorld.py
@@ -1,13 +1,15 @@
 import streamlit as st
 from core import tab_hatching, tab_gap, tab_team
 
+Time_Now = datetime.now() + timedelta(hours=7)
+
 def main():
     st.title("Streamlit App")
 
     tabs = st.tabs(["Hatching Time", "Timers' Gap Balance", "Team Building"])
 
     with tabs[0]:
-        tab_hatching.render()
+        tab_hatching.render(Time_Now)
 
     with tabs[1]:
         tab_gap.render()
