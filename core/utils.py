4f2b7f206cf50c7e7de63338c79918756b770864
trainsr-hub <duoclust@gmail.com>
2026-02-17 20:55:36 +0700

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -185,7 +185,7 @@ def show_graph(C, x_point=None, y_point=None, Optimal_x=None):
                 y[golden_mask],
                 hatch='*',          
                 facecolor='none',
-                edgecolor='#007fff',       # màu vàng golden
+                edgecolor='#8a2be2',       # màu vàng golden
                 linewidth=0
             )
 
